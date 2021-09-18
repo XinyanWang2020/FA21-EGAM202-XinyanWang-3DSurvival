1e2c3d01216f8e3ea0cd49c4044d69b269388b75
XinyanWang2020 <70995686+XinyanWang2020@users.noreply.github.com>
2021-09-18 21:28:14 +0800

    1

--- Assets/Scrips/AvatarController.cs
+++ Assets/Scrips/AvatarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AvatarController : MonoBehaviour
 {
     public GameObject MouseBox;
+    public Animator thisAnimator;
     public UnityEngine.AI.NavMeshAgent thisNavMeshAgent;
 
     public enum MovementStateT { StandStill, walking }
@@ -28,8 +29,20 @@ public class AvatarController : MonoBehaviour
         Physics.Raycast(rayFromCameraToMouse, out closestClickableGroundOnRay, Mathf.Infinity, LayerMask.GetMask("ClickableGround"));
         MouseBox.transform.position = closestClickableGroundOnRay.point;
 
+        //Make MouseBox change size based on mouse button state
+        if (Input.GetMouseButton(0))
+            //When left button is down
+            MouseBox.transform.localScale = new Vector3(10, 10, 10);
+        else
+            //When left button is up
+            MouseBox.transform.localScale = new Vector3(5, 5, 5);
+
         //Tell Avatar's NavMeshAgent to go to MouseBox
         if (Input.GetMouseButtonDown(0))
             thisNavMeshAgent.SetDestination(MouseBox.transform.position);
+
+        //Make animation speed match movement speed
+        GetComponent<Animator>().SetFloat("WalkSpeed", .2f * thisNavMeshAgent.velocity.magnitude);
+
     }
 }
