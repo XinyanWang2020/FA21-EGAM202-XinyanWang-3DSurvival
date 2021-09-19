4d618cb021865fa791ad24dd4863e8e63d7b469d
XinyanWang2020 <70995686+XinyanWang2020@users.noreply.github.com>
2021-09-19 21:43:44 +0800

    1

--- Assets/Scrips/OpenDoor.cs
+++ Assets/Scrips/OpenDoor.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
+
 {
+    public GameObject Button;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter(BoxCollider other)
+    {
+        if (other.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider")
+        {
+            anim.SetTrigger("Open");
+        }
     }
 }
