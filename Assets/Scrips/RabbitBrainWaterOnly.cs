3078cb5f463637b6a4f24a6d6aac1c0471bf47b3
XinyanWang2020 <70995686+XinyanWang2020@users.noreply.github.com>
2021-09-30 10:29:03 +0800

    1

---
+++ Assets/Scrips/RabbitBrainWaterOnly.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RabbitBrainWaterOnly : MonoBehaviour
+{
+    public enum RabbitStateT
+    {
+        DecidingWhatToDoNext,
+        SeekingWater, MovingToWater,DrinkingTillFull,
+        Resting,
+    }
+
+    public float Water = 50, WaterLostPerSecond = 1, DrinkingSpeed = 10;
+    public float Food = 50, FoodLostPerSecond = 1, EatingSpeed = 10;
+
+    public RabbitStateT currentState;
+
+    NavMeshAgent thisNavMeshAgent;
+    void Start()
+    {
+        Water = Random.Range(40f, 60f);
+        Food = Random.Range(40f, 60f);
+        thisNavMeshAgent = GetComponent<NavMeshAgent>();
+
+        currentState = RabbitStateT.DecidingWhatToDoNext;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sun water and food levels decrease
+        Water -= WaterLostPerSecond * Time.deltaTime;
+        Food -= FoodLostPerSecond * Time.deltaTime;
+
+        //Dose sun have enough food and water to live?
+        if (Food < 0 || Water <0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        switch (currentState)
+        {
+            case RabbitStateT.DecidingWhatToDoNext:
+                DecideWhatToDoNext();
+                break;
+            case RabbitStateT.SeekingWater:
+                SeekWater();
+                break;
+            case RabbitStateT.MovingToWater:
+                //nothing needs to do
+                break;
+            case RabbitStateT.DrinkingTillFull:
+                DrinkFromWater();
+                break;
+
+            default:
+                throw new System.NotImplementedException("Whoops. Ran off the end of the case statement....");
+        }
+    }
+
+    public void DecideWhatToDoNext()
+    {
+        if (Water < 50)
+        {
+            currentState = RabbitStateT.SeekingWater;
+            return;
+        }
+
+    }
+
+    public void SeekWater()
+    {
+        GameObject[] wateryObjects = GameObject.FindGameObjectsWithTag("Water");
+        GameObject targetWateryObject = wateryObjects[0];
+        Debug.Log("Rabbit is going to" + targetWateryObject.name);
+
+        //change state to "movingToWater"
+        thisNavMeshAgent.SetDestination(targetWateryObject.transform.position);
+        currentState = RabbitStateT.MovingToWater;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Rabbit EnterTrigger");
+        if(currentState == RabbitStateT.MovingToWater && other.tag == "Water")
+        {
+            currentState = RabbitStateT.DrinkingTillFull;
+        }
+    }
+
+    public void DrinkFromWater()
+    {
+        Water += DrinkingSpeed * Time.deltaTime;
+
+        if (Water > 100)
+            currentState = RabbitStateT.DecidingWhatToDoNext;
+    }
+}
--- Assets/Scrips/SunBrain.cs
+++ Assets/Scrips/SunBrain.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class SunBrain : MonoBehaviour
 {
+    public enum SunfieldStateT { Seed, Seeding, Adult, Flowering, Dead }
+    public SunfieldStateT currentState;
 
+    public int SunfieldID;
+    public float Food, SeedFood, FoodGainedPerSecond, MaxFood;
+    public float CrowdingDistance, MaxDispersalDistance;
+    public float BirthTime, Age;
+
+    public GameObject SunfieldPrefab;
     void Start()
     {
         
